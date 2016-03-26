4e9f9ef5a747200a945355335365862431a91050
linux-jedi <cbthomas1997@gmail.com>
2016-03-26 14:13:15 -0500

    Added untested golang voting script

---
+++ VotingChallenge/vote.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+func postForm() {
+	for {
+		host := "http://localhost/Voting_Challenge.html"
+		resp, err := http.PostForm(host,
+			url.Values{ "name": {"Bishop"}, "email": {"a11Id01sW1n@gmail.com"}} )
+	}
+}
+
+func main(){
+	for i:= 0; i < 100; i++ {
+		go postForm()
+	}
+	for {
+
+	}
+}
