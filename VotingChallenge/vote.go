e1d34521a5959c29f3919e06a851353ba4d15894
linux-jedi <cbthomas1997@gmail.com>
2016-03-27 20:37:50 -0500

    Voting script tested and now uses GET Requests

--- VotingChallenge/vote.go
+++ VotingChallenge/vote.go
@@ -6,18 +6,44 @@ import (
 	"net/url"
 )
 
-func postForm() {
+func votePost() {
 	for {
-		host := "http://localhost/Voting_Challenge.html"
+		host := "http://192.168.0.14/Voting_Challenge.html"
 		resp, err := http.PostForm(host,
-			url.Values{ "name": {"Bishop"}, "email": {"a11Id01sW1n@gmail.com"}} )
+			url.Values{ "name": {"bishop"}, "email": {"a11Id01sW1n@gmail.com"}} )
+		
+		if err != nil {
+			fmt.Println(err)
+		}
+		
+		resp.Body.Close()
+	}
+}
+
+func voteGet() {
+	host := "http://192.168.0.14/welcome_get.php?name=bishop&email=a11Id01sW1n@gmail.com"
+	for {
+		resp, err := http.Get(host)
+		
+		if err != nil {
+			fmt.Println(err)
+		}
+		
+		resp.Body.Close()
+	}
+}
+
+func voteGetFast() {
+	host := "http://192.168.0.14/welcome_get.php?name=bishop&email=a11Id01sW1n@gmail.com"
+	for {
+		http.Get(host)
 	}
 }
 
 func main(){
-	for i:= 0; i < 100; i++ {
+	for i:= 0; i < 10000; i++ {
 		fmt.Println("Starting thread " , i)
-		go postForm()
+		go voteGetFast()
 	}
 	for {
 
