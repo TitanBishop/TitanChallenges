7ad9a9cbc0cc1f90c63bd0672d578792c31b5ca6
linux-jedi <cbthomas1997@gmail.com>
2016-03-28 18:37:05 -0500

    Target is now set to second cmd argument

--- VotingChallenge/vote.go
+++ VotingChallenge/vote.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 )
 
-func voteGetFast() {
-	host := "http://192.168.0.14/welcome_get.php?name=bishop&email=a11Id01sW1n@gmail.com"
-	http.Get(host)
+func voteGetFast(target string) {
+	//target := "http://192.168.0.14/welcome_get.php?name=bishop&email=a11Id01sW1n@gmail.com"
+	http.Get(target)
 }
 
 func main(){
+	var target string = flag.Arg(1)
+
 	for i:= 0; ; i++ {
 		fmt.Println("Starting thread " , i)
-		go voteGetFast()
+		go voteGetFast(target)
 	}
 	for {
 
