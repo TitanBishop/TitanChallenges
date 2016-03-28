cfa82442890e013bf2cbc0bb22d9b7aa7f1cf3e7
linux-jedi <cbthomas1997@gmail.com>
2016-03-27 22:09:18 -0500

    Optimized for more requests per second. Also cleaned up unused methods

--- VotingChallenge/vote.go
+++ VotingChallenge/vote.go
@@ -3,45 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"net/url"
 )
-
-func votePost() {
-	for {
-		host := "http://192.168.0.14/Voting_Challenge.html"
-		resp, err := http.PostForm(host,
-			url.Values{ "name": {"bishop"}, "email": {"a11Id01sW1n@gmail.com"}} )
-		
-		if err != nil {
-			fmt.Println(err)
-		}
-		
-		resp.Body.Close()
-	}
-}
-
-func voteGet() {
-	host := "http://192.168.0.14/welcome_get.php?name=bishop&email=a11Id01sW1n@gmail.com"
-	for {
-		resp, err := http.Get(host)
-		
-		if err != nil {
-			fmt.Println(err)
-		}
-		
-		resp.Body.Close()
-	}
-}
 
 func voteGetFast() {
 	host := "http://192.168.0.14/welcome_get.php?name=bishop&email=a11Id01sW1n@gmail.com"
-	for {
-		http.Get(host)
-	}
+	http.Get(host)
 }
 
 func main(){
-	for i:= 0; i < 10000; i++ {
+	for i:= 0; ; i++ {
 		fmt.Println("Starting thread " , i)
 		go voteGetFast()
 	}
